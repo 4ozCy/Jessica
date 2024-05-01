7dacd5485f4a25ed3117e3d9c0cb0ec2b41b3cc5
Wiston <157526994+Zyneeee@users.noreply.github.com>
2024-05-01 21:07:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -272,7 +272,7 @@ async def on_message(message):
         minutes, seconds = divmod(remainder, 60)
         await message.channel.send(f"{mention.display_name} is AFK: {afk_users[mention.id]['reason']} - AFK for {hours} hours, {minutes} minutes.")
 
-    await bot.process_commands(message)
+    await client.process_commands(message)
 
                 
 client.run(os.getenv('TOKEN'))
