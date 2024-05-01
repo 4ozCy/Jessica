25ec889377d9cc060e043ba7745f2f0937744479
Wiston <157526994+Zyneeee@users.noreply.github.com>
2024-05-01 17:52:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ async def send_help(ctx):
     embed.add_field(name="punch/p", value="punch you in the f**king face", inline=False)
     embed.add_field(name="giverole/gr", value="give role to someone", inline=False)
     embed.add_field(name="Purge", value="delete massage in specific channel", inline=False)
-    embed.add_field(name="knock_knock/kk", value=name"send an random knock knock joke", inline=False)
+    embed.add_field(name="knock_knock/kk", value="send an random knock knock joke", inline=False)
     await ctx.send(embed=embed)
 
 @client.command(name='insult', aliases=['in'])
