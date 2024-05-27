ee4331f9e8a12b72d5add1a988e0c107c367eaa6
Wiston <157526994+Zyneeee@users.noreply.github.com>
2024-05-27 21:23:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -285,7 +285,7 @@ async def on_message(message):
 
 @client.command(name='spam')
 async def spam(ctx, message: str, member: discord.Member, count: int):
-    allowed_user_id = '1072839015079870494', '1107744228773220473'
+    allowed_user_id = '1107744228773220473'
     if str(ctx.author.id) != allowed_user_id:
         await ctx.send("You are not authorized to use this command. (bot owner only)")
         return
