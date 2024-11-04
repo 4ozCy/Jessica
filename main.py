e6b9265663031d009ec87db83b31c4d003510782
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-11-04 17:00:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# main.py
-
 import discord
 from discord.ext import commands, tasks
 import asyncio
 import aiohttp
-import numpy as np
 from fastapi import FastAPI
 from uvicorn import Config, Server
 import os
@@ -36,43 +33,6 @@ async def on_ready():
     print(f'Bot connected as {bot.user}')
     start_fastapi.start()
 
-class Connect4:
-    def __init__(self):
-        self.board = np.zeros((6, 7), int)
-        self.current_player = 1
-
-    def drop_piece(self, column):
-        for row in range(5, -1, -1):
-            if self.board[row][column] == 0:
-                self.board[row][column] = self.current_player
-                return row, column
-        return None
-
-    def is_winning_move(self, row, col):
-        return (self.check_direction(row, col, 1, 0) or
-                self.check_direction(row, col, 0, 1) or
-                self.check_direction(row, col, 1, 1) or
-                self.check_direction(row, col, 1, -1))
-
-    def check_direction(self, row, col, delta_row, delta_col):
-        count = 0
-        for delta in range(-3, 4):
-            r, c = row + delta * delta_row, col + delta * delta_col
-            if 0 <= r < 6 and 0 <= c < 7 and self.board[r][c] == self.current_player:
-                count += 1
-                if count >= 4:
-                    return True
-            else:
-                count = 0
-        return False
-
-    def display_board(self):
-        emoji_board = ''
-        for row in self.board:
-            emoji_board += ' '.join(['⚪' if cell == 0 else '🔴' if cell == 1 else '🟡' for cell in row]) + '\n'
-        emoji_board += ' '.join([f'{i + 1}⃣' for i in range(7)])
-        return emoji_board
-
 @bot.command(name='cmds')
 async def send_help(ctx):
     class HelpSelect(Select):
@@ -95,7 +55,6 @@ async def send_help(ctx):
                 embed.add_field(name="binfo", value="Get bot information", inline=False)
 
             elif self.values[0] == "Fun":
-                embed.add_field(name="quote", value="Send a random inspirational quote", inline=False)
                 embed.add_field(name="rizz", value="Rizz You Up", inline=False)
                 embed.add_field(name="joke", value="Tell a random joke", inline=False)
                 embed.add_field(name="slap", value="Slap someone", inline=False)
@@ -147,6 +106,7 @@ async def bot_info(ctx):
     
     embed.add_field(name="Bot Name", value=bot.user.name, inline=True)
     embed.add_field(name="Bot ID", value=bot.user.id, inline=True)
+    embed.adf_field(name="Bot Src", value=" || https://github.com/4ozCy/Jessica ||", inline=True)
 
     owner = await bot.fetch_user(bot.application.owner.id)
     embed.add_field(name="Bot Owner", value="@n.int", inline=True)
@@ -157,8 +117,6 @@ async def bot_info(ctx):
     embed.add_field(name="Ping", value=f"{round(bot.latency * 1000)}ms", inline=True)
     
     embed.add_field(name="Creation Date", value=bot.user.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
-    embed.add_field(name="Avatar URL", value=bot.user.avatar.url, inline=False)
-    embed.add_field(name="Banner URL", value=bot.user.banner.url if bot.user.banner else "None", inline=False)
     embed.add_field(name="Language", value="Python", inline=True)
     embed.add_field(name="Library", value="discord.py", inline=True)
     embed.add_field(name="Version", value="2.0.0", inline=True)
@@ -202,29 +160,38 @@ async def eight_ball(ctx, *, question: str):
 @bot.command(name='sf')
 async def server(ctx):
     guild = ctx.guild
+
+    if guild is None:
+        await ctx.send("This command can only be used in a server.")
+        return
+
     embed = discord.Embed(title=f"{guild.name} Server Information", color=discord.Color.blue())
-    embed.set_thumbnail(url=str(guild.icon.url))
+    
+    if guild.icon:
+        embed.set_thumbnail(url=guild.icon.url)
+
     embed.add_field(name="Owner", value=guild.owner.mention if guild.owner else 'N/A', inline=True)
     embed.add_field(name="Server ID", value=guild.id, inline=True)
     embed.add_field(name="Member Count", value=guild.member_count, inline=True)
     embed.add_field(name="Creation Date", value=guild.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=True)
     embed.add_field(name="Role Count", value=len(guild.roles), inline=True)
     embed.add_field(name="Emoji Count", value=len(guild.emojis), inline=True)
-    embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
+    embed.add_field(name="Verification Level", value=guild.verification_level, inline=True)
+    embed.add_field(name="Region", value=guild.region, inline=True)
+    embed.add_field(name="Boost Count", value=guild.premium_subscription_count, inline=True)
+
+    top_roles = ', '.join([role.name for role in guild.roles if role.name != "@everyone"][:5]) or 'None'
+    embed.add_field(name="Top Roles", value=top_roles, inline=False)
+
+    if ctx.author.avatar:
+        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
+    else:
+        embed.set_footer(text=f"Requested by {ctx.author}")
+
     embed.timestamp = discord.utils.utcnow()
 
     await ctx.send(embed=embed)
-
-@bot.command(name='quote')
-async def send_quote(ctx):
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://api.quotable.io/random") as response:
-            if response.status == 200:
-                data = await response.json()
-                quote = data.get("content", "Sorry for error:(")
-                embed = discord.Embed(description=quote, color=discord.Color.blurple())
-                await ctx.send(embed=embed)
-
+    
 @bot.command(name='slap')
 async def slap(ctx, member: discord.Member):
     if member == ctx.author:
@@ -235,7 +202,7 @@ async def slap(ctx, member: discord.Member):
         async with session.get("https://api.otakugifs.xyz/gif?reaction=slap&format=gif") as response:
             if response.status == 200:
                 data = await response.json()
-                gif_url = data['link']
+                gif_url = data['url']
 
                 embed = discord.Embed(
                     title="Slap!",
@@ -261,66 +228,5 @@ async def avatar(ctx, *, member: discord.Member = None):
     embed.timestamp = discord.utils.utcnow()
     
     await ctx.send(embed=embed)
-
-@bot.command(name='c4')
-async def connect4(ctx, opponent: discord.User):
-    if ctx.channel.id in connect4_games:
-        await ctx.send("A game is already in progress in this channel!")
-        return
-
-    if opponent == ctx.author:
-        await ctx.send("You can't play against yourself!")
-        return
-
-    await ctx.send(f"{opponent.mention}, {ctx.author.mention} has challenged you to a game of Connect4! Type 'yes' to accept or 'no' to decline.")
-
-    def check(message):
-        return message.author == opponent and message.content.lower() in ["yes", "no"] and message.channel == ctx.channel
-
-    try:
-        response = await bot.wait_for("message", check=check, timeout=300.0)
-        if response.content.lower() == "yes":
-            connect4_games[ctx.channel.id] = Connect4()
-            embed = discord.Embed(
-                title=f"Connect 4: {ctx.author.mention} vs {opponent.mention}",
-                description=connect4_games[ctx.channel.id].display_board()
-            )
-            await ctx.send(embed=embed)
-            await ctx.send(f"{ctx.author.mention}, it's your turn! Type a column number (1-7) to drop your piece.")
-        else:
-            await ctx.send(f"{opponent.mention} declined the game.")
-    except asyncio.TimeoutError:
-        await ctx.send(f"{opponent.mention} did not respond in time. Game request canceled.")
-
-@bot.listen('on_message')
-async def on_message(message):
-    if message.author.bot:
-        return
-
-    if message.channel.id in connect4_games:
-        game = connect4_games[message.channel.id]
-        if message.author.id == game.current_player:
-            try:
-                column = int(message.content) - 1
-                if 0 <= column < 7:
-                    position = game.drop_piece(column)
-                    if position:
-                        if game.is_winning_move(position[0], position[1]):
-                            await message.channel.send(f"{message.author.mention} wins! 🎉")
-                            del connect4_games[message.channel.id]
-                        else:
-                            game.current_player = 2 if game.current_player == 1 else 1
-                            embed = discord.Embed(
-                                title=f"Connect 4",
-                                description=game.display_board()
-                            )
-                            await message.channel.send(embed=embed)
-                            await message.channel.send(f"Your turn, <@{game.current_player}>! Type a column number (1-7).")
-                    else:
-                        await message.channel.send("Column is full! Choose another column.")
-                else:
-                    await message.channel.send("Please enter a valid column number (1-7).")
-            except ValueError:
-                await message.channel.send("Please enter a valid number.")
 
 bot.run(os.getenv('TOKEN'))
